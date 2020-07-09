073b896a8daae99491ced7274c59d547f3d4efee
Israel Canessa <israellcanessa@gmail.com>
2020-07-09 10:27:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,8 @@ def reverse_each_word(string)
   string = string.split(" ")
   new_array = []
     string.each do |word|
-      binding.pry
       "#{word}".reverse.unshift(new_array)
+      [ry]
     end
 
 end
