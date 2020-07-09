670958de3aacdf069904d064ae9d651df5a2db2a
Israel Canessa <israellcanessa@gmail.com>
2020-07-09 10:44:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 def reverse_each_word(string)
 string = string.split(" ")
-new_string = ","
+new_array = []
   string.each do |word|
-    new_string << "#{word}".reverse
+    new_array << "#{word}".reverse
   end
-new_string
+new_array
 end
